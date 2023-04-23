efa586b31b3f8b2c19db5f5b4c3d84674922fda3
Mozzer2310 <sammorris2310@hotmail.com>
2023-04-23 19:43:17 +0100

    Add code for data reliabiltiy experiment

---
+++ scripts/data-reliability-generation.py
@@ -0,0 +1,44 @@
+import csv
+import random
+import pandas
+
+fields = ["video_id", "video_name",
+              "comment_id", "comment", "username", "class"]
+filepath = "../data/reliability/"
+
+if __name__ == "__main__":
+    labellers = ["sam", "tony"]
+    # labellers = ["sam"]
+
+    for labeller in labellers:
+        spam = []
+        ham = []
+        filename = "../data/labelled/" + labeller + "-dataset-labelled.csv"
+        with open(filename, 'r') as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=fields)
+            for row in reader:
+                if row['class'] == "spam":
+                    spam.append(row)
+                elif row['class'] == "ham":
+                    ham.append(row)
+
+        sampled_ham = random.sample(ham, 90)
+        sampled_spam = random.sample(spam, 10)
+
+        new_data = sampled_ham + sampled_spam
+        random.shuffle(new_data)
+        copy = labellers.copy()
+        copy.remove(labeller)
+        out_file = filepath + copy[0] + "-reliability.csv"
+        with open(out_file, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fields)
+            writer.writeheader()
+            for item in new_data:
+                writer.writerow({
+                    "video_id": item["video_id"],
+                    "video_name": item["video_name"],
+                    "comment_id": item["comment_id"],
+                    "comment": item["comment"],
+                    "username": item["username"],
+                    "class": 0
+                })
